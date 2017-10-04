067726144676e18069a1ccb857f4ee0f58c1983d
Shaohy <shy757@icloud.com>
2017-10-04 16:35:48 +0800

    first commit

---
+++ binary_phrases.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+# 打开并读取txt文件
+# 对文本进行清洗，去掉多余的字符'― ―'（采用re）
+# 按照中文符号分割文本
+# 按照空格分割单个文本
+# 将单个文本连接起来，组成二元词组，并保存到列表单中
+# 统计列表中相同二元词组出现的频率
+# 输出频率最高的前 10 个「二元词组」
+
+import os
+import re
+from collections import Counter
+from zhon.hanzi import punctuation
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+file_dir = os.path.join(basedir, 'happiness_seg.txt')
+with open(file_dir, 'r', encoding = 'utf8') as f:  # 打开并读取txt文件
+    text = f.read()
+line1 = re.sub('― ―', '', text)  # 对文本进行清洗，去掉多余的字符'― ―'（采用re）
+line2 = re.split(r'[{}]'.format(punctuation), line1)  # 按照中文符号分割文本
+a = []
+for item in line2:  # item 为按中文符号分割后的单个文本
+    phrases = item.split()  # word 为按单个文本中的空格分割出的词组列表
+    for i in range(len(phrases)-1):
+        binary_phrases = phrases[i] + " " + phrases[i+1]  # 连接为二元词组
+        a.append(binary_phrases)  # 保存到列表
+
+print(Counter(a).most_common(10))  # 统计列表中相同元素的频率并输出前十个最高的
+
+# 错误的方法——re方法，没办法把"今天 天气 不错"分割成两个二元词组
+# words = re.findall(r'\w+\s\w+', open(file_dir, 'r', encoding = 'utf8').read())
+# print(Counter(words).most_common(10))
